d57af9c6ddfa942e4d08b3e68b0b28d2f0f14d6d
spellbinder1 <68787156+spellbinder1@users.noreply.github.com>
2020-10-07 21:39:21 +0530

    Create Bellmanford.cpp

---
+++ Bellmanford.cpp
@@ -0,0 +1,48 @@
+#include "bits/stdc++.h"
+using namespace std;
+vector<tuple<int,int,int> >edge;
+
+int main(int argc, char const *argv[])
+{ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+ 
+
+//bellman-ford algorithm
+//we will use here edge list
+
+//each item will store b/w what points is the
+//edge located and weight of edge
+
+
+int n,m;//n is the vertex and m is the number of edges
+//TIME COMPLEXITY IS O(nm);
+//Bellman ford also works for negative edges unlike dijstra
+
+for(int i=0;i<m;i++)
+{
+  int a,b,w;
+  cin>>a>>b>>w;
+  v1.pb(make_tuple(a,b,w));
+}
+
+int dist[n+1];//it will tell the distance of each node from source
+
+//implementing BELLMAN-FORD here
+for(int i=0;i<n;i++)
+{
+  for(auto e:edge)
+  {
+    int a,b,w;
+    tie(a,b,w)=e;
+    dist[b]=min(dist[b],dist[a]+w);
+  }
+}
+
+
+
+//what happens is in each iteration of this algo
+//it tries to improve the distance of a vertex 
+//it's also single source shotest path algo like dijkstra
+
+}
