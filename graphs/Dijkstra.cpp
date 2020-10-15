1a5668dc38ba8f6732a9da1a0051dc9447d570a1
spellbinder1 <68787156+spellbinder1@users.noreply.github.com>
2020-10-15 12:05:19 +0530

    Update Dijkstra.cpp

--- graphs/Dijkstra.cpp
+++ graphs/Dijkstra.cpp
@@ -1,27 +1,5 @@
-#include "bits/stdc++.h"
-using namespace std;
-typedef long long int ll;
-#define mod 1000000007
-const int N=1e5;
-#define mp make_pair
-#define ip pair<int,int>
-#define umii unordered_map<int,int>
-#define umss unordered_map<string,string>
-#define FOR(i,a,b) for(ll i=a;i<=b;i++)
-#define lcm(a,b) (a*b)/gcd(a,b)
-#define all(x) x.begin(),x.end()
-#define setbit(x) __builtin_popcount(x)
-#define print(x) for(auto i:x)cout<<i<<" ";
-#define pb push_back
-const ll INF=1e9;
 vector<vector<ip> >v(N+1);//first store vertex second weight of edge 
-
-
-
 ///dijkstra fails if edges have negative weights
-
-
-
 void dijkstra(int src)
 {
   
@@ -51,9 +29,4 @@ void dijkstra(int src)
  
 
 
-}
-int main(int argc, char const *argv[])
-{
-  
-//dijkstra algorithm
 }
