5e039d0f27503ead70bbcdc4af0eb883dfbe0bbf
spellbinder1 <68787156+spellbinder1@users.noreply.github.com>
2020-10-20 17:11:54 +0530

    Update Bellmanford.cpp

--- graphs/Bellmanford.cpp
+++ graphs/Bellmanford.cpp
@@ -1,20 +1,4 @@
-#include "bits/stdc++.h"
-using namespace std;
-typedef long long int ll;
-const ll mod=1e9+7;
-const int N=1e5;
-#define mp make_pair
-#define ip pair<int,int>
-#define umii unordered_map<int,int>
-#define umss unordered_map<string,string>
-#define FOR(i,a,b) for(ll i=a;i<=b;i++)
-#define lcm(a,b) (a*b)/gcd(a,b)
-#define all(x) x.begin(),x.end()
-#define setbit(x) __builtin_popcount(x)
-#define print(x) for(auto i:x)cout<<i<<" ";
-#define pb push_back
 vector<tuple<int,int,int> >edge;
-
 int main(int argc, char const *argv[])
 { 
  ios_base::sync_with_stdio(false);
